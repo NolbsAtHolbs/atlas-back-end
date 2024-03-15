a5a252b533744402ed5424daeaea4c42591137ec
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2024-03-15 13:56:04 -0500

    2 print

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -9,28 +9,21 @@ import sys
 
 
 if __name__ == "__main__":
-    APIURL = "https://jsonplaceholder.typicode.com/"
-    EmployeeID = sys.argv[1]
-    EmployeeName = requests.get(APIURL + "users/" + EmployeeID)
-    EmployeeJSONDump = EmployeeID.json()
-    EmployeeJSONDumpName: str = EmployeeJSONDump.get("name")
-    EmployeeToDos = requests.get(APIURL + "users/" + EmployeeID + "/todos/")
-    EmployeeToDosJSONDump = EmployeeToDos.json()
+    APIURL = 'https://jsonplaceholder.typicode.com/'
+    EmployeeName = requests.get(APIURL + '/users/{}'.format(sys.argv[1]))
+    EmployeeJD = EmployeeName.json()
+    ToDo = requests.get(APIURL + '/users/{}'.format(sys.argv[1]) + '/todos')
+    ToDos = ToDo_jd.json()
+    ToDone = 0
+    TotalToDos = 0
+    for ToDo in ToDos:
+        TotalToDos = TotalToDos + 1
+        if ToDo["ToDone"] is True:
+            ToDone = ToDone + 1
 
-    EmployeeTotalToDos = 0
-    for ToDo in EmployeeToDosJSONDump:
-        if ToDo["EmployeeID"] == EmployeeID:
-            EmployeeTotalToDos = EmployeeTotalToDos + 1
+    print("Employee {0} is done with tasks({1}/{2}):".format(
+        EmployeeJD["name"], ToDone, TotalToDos))
 
-    EmployeeToDones = 0
-    EmployeeToDoneCount = []
-    for ToDo in EmployeeToDosJSONDump:
-        if ToDo["EmployeeID"] == EmployeeID and ToDo["Done"] is True:
-            EmployeeToDones = EmployeeToDones + 1
-            EmployeeToDoneCount.append(EmployeeToDos["EmployeeToDoName"])
-
-    PrintEmployeeToDones: str = f"Employee {EmployeeName} is done with tasks( \
-        {EmployeeToDones}/{EmployeeTotalToDos}):"
-    EmployeeToDoneCount.insert(PrintEmployeeToDones)
-
-    print(*EmployeeToDoneCount, sep='\n\t')
+    for ToDo in ToDos:
+        if ToDo["ToDone"] is True:
+            print("\t {}".format(ToDo["title"]))
