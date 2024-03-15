8546ed67dc222d688bc3b250fd7fda6b74d4d284
NolbsAtHolbs <hikikomode@gmail.com>
2024-03-15 13:59:08 -0500

    proper name scheme

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -14,16 +14,16 @@ if __name__ == "__main__":
     EmployeeJD = EmployeeName.json()
     ToDo_jd = requests.get(APIURL + '/users/{}'.format(sys.argv[1]) + '/todos')
     ToDos = ToDo_jd.json()
-    ToDone = 0
+    completed = 0
     TotalToDos = 0
     for ToDo in ToDos:
         TotalToDos = TotalToDos + 1
-        if ToDo["ToDone"] is True:
-            ToDone = ToDone + 1
+        if ToDo["completed"] is True:
+            completed = completed + 1
 
     print("Employee {0} is done with tasks({1}/{2}):".format(
-        EmployeeJD["name"], ToDone, TotalToDos))
+        EmployeeJD["name"], completed, TotalToDos))
 
     for ToDo in ToDos:
-        if ToDo["ToDone"] is True:
+        if ToDo["completed"] is True:
             print("\t {}".format(ToDo["title"]))
