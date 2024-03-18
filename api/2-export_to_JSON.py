e21bc7a0984c7182d2e2daad9ad5cb0e876a3925
NolbsAtHolbs <hikikomode@gmail.com>
2024-03-18 14:44:02 -0500

    json export, mind file

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -8,3 +8,17 @@ import sys
 
 
 if __name__ == "__main__":
+    APIURL = "https://jsonplaceholder.typicode.com/"
+    FileName = "{}.json".format(sys.argv[1])
+    EmployeeName = requests.get(APIURL + '/users/{}'.format(sys.argv[1]))
+    EmployeeJD = EmployeeName.json()
+    ToDos = requests.get(APIURL + "/users/{}".format(sys.argv[1]) + "/todos")
+    ToDosJD = ToDos.json()
+    for ToDo in ToDosJD:
+        TasksData = {
+            "USER_ID": [{"task": ToDo["title"],
+                    "completed": ToDo["completed"],
+                    "username": EmployeeJD["name"]}]
+                    }
+    with open(f"{user_id}.json", "w") as file:
+        json.dump(TasksData, file)
