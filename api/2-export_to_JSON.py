d753409c10eadd3dffac7031dcc619e42f3cc1c0
NolbsAtHolbs <hikikomode@gmail.com>
2024-03-18 16:16:47 -0500

    var

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -10,7 +10,7 @@ import sys
 if __name__ == "__main__":
     APIURL = "https://jsonplaceholder.typicode.com/"
     FileName = "{}.json".format(sys.argv[1])
-    EmployeeName = requests.get(APIURL + '/users/{}'.format(sys.argv[1]))
+    EmployeeName = requests.get(APIURL + "/users/{}".format(sys.argv[1]))
     EmployeeJD = EmployeeName.json()
     ToDos = requests.get(APIURL + "/users/{}/todos".format(sys.argv[1]))
     ToDosJD = ToDos.json()
